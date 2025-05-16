25032374a8a94a98a86ed8ed8660ee23c23002c6
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-05-16 13:48:39 +0000

    fixing pycodestyle and some error with the last tasks

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if not a_dictionary:
+        return None
+    best_key = None
+    best_value = float('-inf')
+
+    for key in a_dictionary:
+        if a_dictionary[key] > best_value:
+            best_value = a_dictionary[key]
+            best_key = key
+
+    return best_key
---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictonary, key, value):
+    a_dictonary[key] = value
+    return a_dictonary
