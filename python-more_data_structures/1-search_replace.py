ca124605e43a5e7a380d0e1f0f5dc7b9c0b9893e
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-05-16 09:39:32 +0000

    modify variable's name

--- python-more_data_structures/1-search_replace.py
+++ python-more_data_structures/1-search_replace.py
@@ -2,7 +2,7 @@
 def search_replace(my_list, search, replace):
     new_list = []
     for item in my_list:
-        if iteam == search:
+        if item == search:
             new_list.append(replace)
         else:
             new_list.append(item)
