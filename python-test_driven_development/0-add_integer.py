34608ba96e42489c171a429be760eeaebb43fa09
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-05-16 21:19:00 +0000

    fix docstring and pycodestyle

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""This module provides a function to add two integers."""
+
+
 def add_integer(a, b=98):
+    """Adds two integers or floats (casted to int) and returns the result."""
     if not isinstance(a, (int, float)):
         raise TypeError("a must be an integer")
     if not isinstance(b, (int, float)):
