88881e6f860d48babbd284f8cb7cbe45fce98c78
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-05-16 09:04:02 +0000

    returns a new matrix

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,9 @@
+#!usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    new matrix = []
+    for row in matrix:
+        new row = []
+        for item in row:
+            new row.append(item ** 2)
+        new matrix.append(new row)
+    return new matrix
