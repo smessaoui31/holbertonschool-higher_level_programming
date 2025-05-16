99ff62fd9d70eb434abba6915ebb833a8b5d5d87
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-05-16 15:00:58 +0000

    fixing the TypeError

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -2,6 +2,9 @@
 def safe_print_integer(value):
     try:
         print("{:d}" .format(value))
-        return True
-    except (ValueError, TypeError):
+    except ValueError:
         return False
+    except TypeError:
+        return False
+    else:
+        return True
