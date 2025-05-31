06270c63d47a96da6e629d908791bbb71fc43ab7
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-05-31 14:17:48 +0000

    Refactor docstring format in lookup function for clarity

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
-"""Return the list of available attributes
-and methods of an object."""
-
 
 def lookup(obj):
+    """Return the list of available attributes and methods of an object."""
     return dir(obj)
