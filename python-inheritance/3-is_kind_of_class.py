2d9265064672226d0b9a602c0a6aace0ad37eefb
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-05-31 14:27:43 +0000

    Update module docstring for clarity and consistency

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""
+This module defines a function that checks if
+ an object is an instance of a class
+or a class that inherited from it.
+"""
+
 
 def is_kind_of_class(obj, a_class):
     """
