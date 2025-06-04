7dc0ad0eb0e188621a8871b77bfd50391dfe2d9d
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-04 09:02:04 +0000

    Refactor read_file function documentation and fix filename parameter usage

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -1,15 +1,20 @@
-#!/usr/bin/python3
-"""Function to open , read and close a file"""
+#!/usr/bn/python3
+"""
+This module provides a function to read and
+print the contents of a UTF-8 text file.
+"""
 
 
 def read_file(filename=""):
     """
-    Read a text file with utf-8
-    filename = the name of the file to read
+    Reads a text file (UTF8) and prints its content to stdout.
+
+    Args:
+        filename (str): The name of the file to read.
 
     Usage example:
     >>> read_file("tests/sample.txt")
     Hello, Holberton!
     """
-    with open('', encoding="utf-8") as f:
+    with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
