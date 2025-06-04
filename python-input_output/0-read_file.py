d52461cf1079fbef113e1c70ad88a1b6d8d292c1
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-04 09:00:14 +0000

    Update docstring in read_file function to include usage example

--- python-input_output/0-read_file.py
+++ python-input_output/0-read_file.py
@@ -6,6 +6,10 @@ def read_file(filename=""):
     """
     Read a text file with utf-8
     filename = the name of the file to read
+
+    Usage example:
+    >>> read_file("tests/sample.txt")
+    Hello, Holberton!
     """
     with open('', encoding="utf-8") as f:
         print(f.read(), end="")
