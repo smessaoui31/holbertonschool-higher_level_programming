fb6b06c6496d13a18e1d84487941d2b076a65a29
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-05 14:01:15 +0000

    Rename function from_json_string to to_json_string for consistency with module purpose

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -5,7 +5,7 @@ Module to convert a JSON string into a Python object.
 import json
 
 
-def from_json_string(my_obj):
+def to_json_string(my_obj):
     """
     Convert JSON string to Python object.
     """
