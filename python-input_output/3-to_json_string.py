13dfcd47a719ea51e6fd6567a8f59888459b2873
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-05 13:45:00 +0000

    Refactor import statement for json module to improve code organization

--- python-input_output/3-to_json_string.py
+++ python-input_output/3-to_json_string.py
@@ -2,11 +2,11 @@
 """
 Module to convert a JSON string into a Python object.
 """
+import json
 
 
 def from_json_string(my_str):
     """
     Convert JSON string to Python object.
     """
-    import json
     return json.loads(my_str)
