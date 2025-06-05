ae81f2d2e0654c222d087eb3dd424e142d38041a
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-05 09:30:58 +0000

    Fix file permissions and clean up docstring formatting in write_file function

--- python-input_output/1-write_file.py
+++ python-input_output/1-write_file.py
@@ -5,8 +5,6 @@ def write_file(filename="", text=""):
     """
 Write a string to a UTF-8 text file and returns
 the number of characters written
-"""
+    """
     with open(filename, "w", encoding="utf-8") as f:
         return f.write(text)
-
-    
