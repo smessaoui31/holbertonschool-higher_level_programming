c618c8868e461663be85c00f13e84ea203617ce9
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-05 09:25:16 +0000

    Add write_file function to write a string to a UTF-8 text file

---
+++ python-input_output/1-write_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def write_file(filename="", text=""):
+    """
+Write a string to a UTF-8 text file and returns
+the number of characters written
+"""
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
+
+    
