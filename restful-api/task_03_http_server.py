c5b9c2dcb642a3950f9fea3473039469164e1180
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-13 07:01:01 +0000

    Add SimpleAPIHandler class to implement a basic HTTP server with GET endpoints

---
+++ restful-api/task_03_http_server.py
@@ -0,0 +1,52 @@
+#!/usr/bin/python3
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import json
+
+# gestionnaire de requêtes HTTP
+
+
+class SimpleAPIHandler(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        if self.path == "/":
+            self.send_response(200)  # = OK
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Hello, this is a simple API!")
+
+        elif self.path == "/data":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")  # Type JSON
+            self.end_headers()
+            data = {
+                "name": "John",
+                "age": 30,
+                "city": "New York"
+            }
+            # Transformer le dictionnaire en texte JSON
+            self.wfile.write(json.dumps(data).encode('utf-8'))
+
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            status = {"status": "OK"}
+            self.wfile.write(json.dumps(status).encode('utf-8'))
+
+        else:
+            self.send_response(404)
+            self.send_header("Content-type", "text/plain")
+            self.end_headers()
+            self.wfile.write(b"Endpoint not found")
+
+
+# Launch the server on the port 8000
+def run():
+    server_address = ('', 8000)
+    httpd = HTTPServer(server_address, SimpleAPIHandler)
+    print("Server running on http://localhost:8000")
+    httpd.serve_forever()
+
+
+if __name__ == "__main__":
+    run()
