f4dfe3f6c9f0e6a08a4232fc7cd0c67be5c34503
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-13 07:32:38 +0000

    adding a condition for the endpoint "/status"

--- restful-api/task_03_http_server.py
+++ restful-api/task_03_http_server.py
@@ -26,6 +26,12 @@ class SimpleAPIHandler(BaseHTTPRequestHandler):
             # Transformer le dictionnaire en texte JSON
             self.wfile.write(json.dumps(data).encode('utf-8'))
 
+        elif self.path == "/status":
+            self.send_response(200)
+            self.send_header("Content-type", "application/json")
+            self.end_headers()
+            self.wfile.write(json.dumps({"status": "OK"}, separators=(',', ':')).encode('utf-8'))
+
         elif self.path == "/info":
             self.send_response(200)
             self.send_header("Content-type", "application/json")
