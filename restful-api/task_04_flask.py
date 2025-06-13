4f25a9f1bf98cea7f782b3022ab6218324b51e69
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-13 20:18:29 +0000

    Refactor user retrieval and addition endpoints for improved structure and error handling

--- restful-api/task_04_flask.py
+++ restful-api/task_04_flask.py
@@ -7,31 +7,55 @@ import json
 app = Flask(__name__)
 users = {}
 
+
 @app.route("/")
 def home():
     return "Welcome to the Flask API!"
+
 
 @app.route("/data")
 def get_usernames():
     usernames = [username for username in users.keys()]
     return jsonify(usernames)
 
+
 @app.route("/users/<username>")
 def get_user(username):
     if username in users.keys():
         user = users[username]
         return jsonify(user)
-    else :
+    else:
         error = {"error": "User not found"}
         return jsonify(error), 404
 
 
+@app.route("/add_user", methods=["POST"])
+def add_user():
+    if request.method == "POST":
+        content = json.loads(request.data)
+
+        if "username" in content.keys():
+            user = {
+                "username": content["username"],
+                "name": content["name"],
+                "age": content["age"],
+                "city": content["city"]
+            }
+            users[user["username"]] = user
+            message = "User added"
+            return jsonify({
+                "message": message,
+                "user": user
+            }), 201
+
+        else:
+            message = {"error": "Username is required"}
+            return jsonify(message), 400
 
 
-
-
-
-
+@app.route("/status")
+def get_status():
+    return ("OK")
 
 
 if __name__ == "__main__":
