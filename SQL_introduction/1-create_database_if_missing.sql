e987d306dac3709629023108ea90f5d910cfe562
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-24 14:59:34 +0000

    Fix comment in SQL script to clarify database creation condition

--- SQL_introduction/1-create_database_if_missing.sql
+++ SQL_introduction/1-create_database_if_missing.sql
@@ -1,3 +1,3 @@
-/* Script to create a database named hbtn_0c_0*/
+/* Script to create a database named hbtn_0c_0 if she doesnt exist*/
 CREATE DATABASE IF NOT EXISTS hbtn_0c_0;
 --The database hbtn_0c_0 is created in my sql server
