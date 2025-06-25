ce8cb6600164018e7040810999299471bab6dae5
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-25 13:57:50 +0000

    Add SQL script to list the number of records with the same score from second_table

---
+++ SQL_introduction/15-groups.sql
@@ -0,0 +1,6 @@
+-- Script to list the number of records with same score
+SELECT score, COUNT(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
+-- Number of record has been printed
