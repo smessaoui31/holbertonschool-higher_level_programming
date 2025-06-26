3c1c569b95d3896712cf7c9d409fe5b79ccc101e
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-26 08:26:50 +0000

    Update SQL script to clarify user creation and privilege granting

--- SQL_more_queries/1-create_user.sql
+++ SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
--- CREATE THE SQL SERVER  USER user_0d_1
+-- CREATE THE SQL SERVER  USER user_0d_1 and grant him all privileges
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost';
 -- The sql server has been created
