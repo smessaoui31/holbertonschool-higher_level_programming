72d2393a534fe4ed366d21d7c9dead2606563e17
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-26 11:28:14 +0000

    Fix formatting in unique_id table creation script

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
     -- Create a table with an unique id 
-    CREATE TABLE IF NOT EXISTS unique_id(
+    CREATE TABLE IF NOT EXISTS unique_id (
         id INT NOT NULL DEFAULT 1 UNIQUE,
         name VARCHAR(256)
     );
-    -- The table unique_id has been created with an unique id 
+    -- The table unique_id has been created with an unique id 1
