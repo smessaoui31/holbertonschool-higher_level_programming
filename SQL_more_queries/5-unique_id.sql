07b5b6d165eafb322e125b8f0464be3e1af5da15
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-06-26 11:49:02 +0000

    Add NOT NULL constraint to id column in unique_id table creation script

--- SQL_more_queries/5-unique_id.sql
+++ SQL_more_queries/5-unique_id.sql
@@ -1,6 +1,6 @@
     -- Create a table with an unique id 
     CREATE TABLE IF NOT EXISTS unique_id (
-        id INT DEFAULT 1 UNIQUE,
+        id INT NOT NULL DEFAULT 1 UNIQUE,
         name VARCHAR(256)
     );
     -- The table unique_id has been created with an unique id 1
