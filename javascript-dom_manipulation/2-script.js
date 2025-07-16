f5efaa29fb40513ff0a3e0bda78b07af5ba2961a
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-07-16 11:52:09 +0000

    Add the class red to the header element when user click on the tag correspond

---
+++ javascript-dom_manipulation/2-script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const redHeader = document.querySelector('#red_header')
+  const header = document.querySelector('header')
+
+  redHeader.addEventListener('click', () => {
+    header.classList.add('red')
+  })
+})
