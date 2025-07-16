e3f1e95d118820e33b333989f0cd3cd4115ee06b
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-07-16 12:05:40 +0000

    feat: fetch and display French translation of "hello" using Fetch API

---
+++ javascript-dom_manipulation/8-script.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const helloElement = document.querySelector('#hello')
+
+  fetch('https://hellosalut.stefanbohacek.dev/?lang=fr')
+    .then(response => response.json())
+    .then(data => {
+      helloElement.textContent = data.hello
+    })
+    .catch(error => {
+      console.error('Error:', error)
+    })
+})
