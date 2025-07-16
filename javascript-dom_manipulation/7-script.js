c8ce3a8e30014938424a7fc23b337f8f92aebde7
smessaoui31 <messaoui.sofian@hotmail.fr>
2025-07-16 12:04:46 +0000

    feat: fetch and display all Star Wars movie titles using Fetch API

---
+++ javascript-dom_manipulation/7-script.js
@@ -0,0 +1,16 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const list = document.querySelector('#list_movies')
+
+  fetch('https://swapi-api.hbtn.io/api/films/?format=json')
+    .then(response => response.json())
+    .then(data => {
+      data.results.forEach(movie => {
+        const li = document.createElement('li')
+        li.textContent = movie.title
+        list.appendChild(li)
+      })
+    })
+    .catch(error => {
+      console.error('Error:', error)
+    })
+})
